f27e3d43b753fd71d49e3df2578ed9efa1b1a14e
Augusto Villar <augusto.villar@usp.br>
2025-03-25 22:22:41 +0100

    delete the node.js backend

--- backend/routes/artRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const artController = require('../controllers/artController');
-
-router.post('/api/search-images', artController.searchImages);
-router.post('/api/generate-story', artController.generateStory);
-router.post('/api/select-images', artController.selectImages);
-
-module.exports = router;
